7c500368ec84e8d7c3dcf62239d1cfe2871e2b52
Kevin Ashworth <kevinashworth@yahoo.com>
2020-06-01 16:25:55 -0700

    Double OverlayExample

--- src/OverlayExample.jsx
+++ src/OverlayExample.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
 import Tooltip from 'react-bootstrap/Tooltip'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 // import MyComponent1 from './MyComponent1'
@@ -12,14 +14,46 @@ function renderTooltip (props) {
   )
 }
 
+function renderCard (props) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+  )
+}
+
 const OverlayExample = () => (
-  <OverlayTrigger
-    placement='bottom'
-    overlay={renderTooltip}
-    trigger='click'
-  >
-    <Button variant='success'>Click to see Toolip</Button>
-  </OverlayTrigger>
+  <>
+    <OverlayTrigger
+      placement='bottom'
+      overlay={renderTooltip}
+      trigger='click'
+    >
+      <Button variant='success'>Click to see Toolip</Button>
+    </OverlayTrigger>
+    <OverlayTrigger
+      placement='bottom'
+      overlay={renderCard}
+      trigger='click'
+    >
+      <Button variant='danger'>Click to see Card</Button>
+    </OverlayTrigger>
+  </>
 )
 
 export default OverlayExample
